483ae5394edd9679258afa14ce2aeb625130afe5
azikodev <aziko.coder@gmail.com>
2024-12-04 12:12:35 +0500

    edition bg color

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -9,7 +9,7 @@ import { Outlet } from "react-router-dom";
 
 function MainLayout() {
   return (
-    <div className="flex-1 h-auto bg-[#eee3e3]">
+    <div className="flex-1 h-auto bg-[#928c8c]">
       <main className="container max-w-[1200px] m-auto">
         <Navbar />
       </main>
